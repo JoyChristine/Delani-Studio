b618139a8a07e9b4015e5f103bafc216b5fae91b
Joy Christine <joychristin2@gmail.com>
2022-03-11 15:02:16 +0300

    add toggle function

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#hide1").hide();
+    $("#click1").click(function() {
+        $("#show1").toggle(700);
+      $("#hide1").toggle(600);
+     
+    });
+    $("#hide2").hide();
+    $("#click2").click(function() {
+        $("#show2").toggle(700);
+      $("#hide2").toggle(600);
+     
+    });
+    $("#hide3").hide();
+    $("#click3").click(function() {
+        $("#show3").toggle(700);
+      $("#hide3").toggle(600);
+     
+    });
+    
+});
