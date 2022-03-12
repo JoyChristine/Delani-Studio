85808e9d42d4533c33ac3ac39bc0f9379bced2de
Joy Christine <joychristin2@gmail.com>
2022-03-12 16:37:51 +0300

    add function when one clicks submit btn

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
+  // what we do
     $("#hide1").hide();
     $("#click1").click(function() {
-      $("#show1").toggle(700);
-      $("#hide1").toggle(600);
+      $("#show1").slideToggle(700);
+      $("#hide1").slideToggle(600);
      
     });
     $("#hide2").hide();
     $("#click2").click(function() {
-      $("#show2").toggle(700);
-      $("#hide2").toggle(600);
+      $("#show2").slideToggle(700);
+      $("#hide2").slideToggle(600);
      
     });
     $("#hide3").hide();
     $("#click3").click(function() {
-      $("#show3").toggle(700);
-      $("#hide3").toggle(600);
+      $("#show3").slideToggle(700);
+      $("#hide3").slideToggle(600);
      
     });
     // portfolio
     $(".card").hover(function() {
       $(this).children(".card-1").fadeToggle(500, "linear");
     })
+    $("button").click(function(event){
+      var person = document.getElementById("name").value;
+      alert('Dear' + " " + person + " "+ 'we have received your message. Thank you for reaching out to us.');
+      event.preventDefault();
+    })
 });
-    
+
+
+
